82979b5f9409efaced486c2ad73d031804c0adfd
Tera <teraclenin@gmail.com>
2018-01-30 20:50:47 -0600

    first commit

---
+++ converterapp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace converterapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("title: converter app");
+            Console.WriteLine("description: converts yards to inches");
+            Console.WriteLine("enter number of yards!");
+
+            int numYards = int.Parse(Console.ReadLine());
+            int numberInches = numYards * 36;
+
+            Console.WriteLine("number of inches in " + numYards + "inches");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
